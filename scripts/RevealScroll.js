2076c0976be3618eefd8ea2909865ff4cecf345f
Bernardo Araujo <bernardoaraujons@gmail.com>
2024-02-15 15:47:52 -0300

    changes

--- scripts/RevealScroll.js
+++ scripts/RevealScroll.js
@@ -1,9 +1,9 @@
 window.scrl = ScrollReveal({
     origin: 'top',
-    distance: '60px',
+    distance: '100px',
     duration: 1500,
     delay: 200,
     reset: true
 })
 
-scrl.reveal('.container, #banner img, #ourMetrics, .divReason, .filter, .technologiesList, .logoFooter, .middleFooter');
+scrl.reveal('.container, #banner img, #ourMetrics, .divReason, .filter, .technologiesList, .logoFooter, .middleFooter, .footerInferior, .mapAndInfos, .contactForm, .allTestimonial');
